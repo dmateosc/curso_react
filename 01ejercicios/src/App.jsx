2b9cd6c5a2b077f2ca38a2ef0c248f2972a2f685
Dmateosca <mateoscano.david@gmail.com>
2020-11-05 19:56:44 +0100

    Subida de la estructura inicial

---
+++ 01ejercicios/src/App.jsx
@@ -0,0 +1,14 @@
+//importación
+import React from 'react'
+import './App.css'
+
+//generación de la función del componente
+const App = () => {
+
+    //Lo que ejecuta la función
+
+    return <div className="react-calculator">Calc App</div>
+}
+
+//exportación
+export default App
---
+++ 01ejercicios/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//importar el componente
+import App from './App'
+
+ReactDOM.render(<App></App>,
+  document.querySelector('#root'))
+
