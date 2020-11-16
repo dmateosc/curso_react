d40e8c6d9b3c6076ba87e43723a9e704e983b308
Dmateosca <mateoscano.david@gmail.com>
2020-11-16 10:44:43 +0100

    Subida con la creación de filas

--- pruebas-codigo/src/App.js
+++ pruebas-codigo/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <>
 
     <Tabla></Tabla>
+    <Tabla></Tabla>
     </>
 
   );
--- pruebas-codigo/src/Tabla/Tabla.jsx
+++ pruebas-codigo/src/Tabla/Tabla.jsx
@@ -1,25 +1,53 @@
-import React, { useState,useEffect } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import Fila from "./../Fila";
 //import { Test } from './Tabla.styles';
 
 const Tabla = (props) => {
   const [rows, setRows] = useState([1]);
   
+
   const addRow = () => {
-    setRows([...rows,rows.length+1]);
+    setRows([...rows, rows.length + 1]);
   };
-  const returnRows = (rows) => {
-      return[
-      <div >
-        <Fila key={rows} serie={rows}></Fila>
+  const deleteRow = (rowValue) => {
+      
+      const rowArr = rows.filter(i => i !== rowValue);
+      setRows(rowArr);
+  };
+  const returnRows = (rowsValue) => {
+    /*var clickAniadir = (
+      <div>
+        <Fila key={rowsValue} serie={rowsValue}></Fila>
         <button onClick={addRow}>Click</button>
-      </div>]
-    };
+      </div>
+    );*/
+    var aniadir = (
+      <div>
+        <Fila key={rowsValue} serie={rowsValue}></Fila>
+      </div>
+    );
+    //IMportante se añade el onclick con () para que la función solo pueda ser llamda una vez has clickado
+    var clickBorrar = (
+      <div>
+        <Fila key={rowsValue} serie={rowsValue}></Fila>
+        <button onClick={addRow}>Click</button>
+        <button onClick={() => deleteRow(rowsValue)}>Borrar</button>
+      </div>
+    );
+    if (rowsValue === rows.length) {
+      return [clickBorrar];
+    } else {
+      return [aniadir];
+    }
+  };
   /* setRows(rows.push(
     {numRows: numRows}
   ))*/
-  return (<>{ rows.map((rows) => returnRows(rows)) }</>);
+  if (rows.length < 1 || rows.length === undefined) {
+    return <></>;
+  } else {
+    return (<>{rows.map((rows) => returnRows(rows))}</>);
+  }
 };
 
 export default Tabla;
